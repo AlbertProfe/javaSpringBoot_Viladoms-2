8dcd0d71b54e15664f89bdda0c0ae7a88f871b61
Albert.Profe <albertprofe@gmail.com>
2022-03-08 19:36:11 +0100

    Commit #4: update and check time operations write-read db on cloud

--- employee/src/main/java/com/example/employee/ApplicationCommandRunner.java
+++ employee/src/main/java/com/example/employee/ApplicationCommandRunner.java
@@ -1,5 +1,6 @@
 package com.example.employee;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -24,9 +25,31 @@ public class ApplicationCommandRunner implements CommandLineRunner {
 		logger.info("Welcome to the runner from commandLineRunner to test JPA mapping 1:n");
 
 		logger.info("Data creation Employee started...");
+		
+		Timestamp startCreate20 = new Timestamp(System.currentTimeMillis());
 		employeeRepository.save(new Employee("Joan", "Jones", 25));
 		employeeRepository.save(new Employee("Anna", "Williams", 28));
-		employeeRepository.save(new Employee("Pere", "Williams", 28));
+		employeeRepository.save(new Employee("Pere", "Williams Sarger", 28));
+		employeeRepository.save(new Employee("Pere", "Williams Sarger", 28));
+		employeeRepository.save(new Employee("Pere", "Williams Sarger", 28));
+		employeeRepository.save(new Employee("Pere", "Williams Sarger", 28));
+		employeeRepository.save(new Employee("Pere", "Williams Sarger", 28));
+		employeeRepository.save(new Employee("Joan", "Jones", 25));
+		employeeRepository.save(new Employee("Anna", "Williams", 28));
+		employeeRepository.save(new Employee("Joan", "Jones", 25));
+		employeeRepository.save(new Employee("Anna", "Williams", 28));
+		employeeRepository.save(new Employee("Joan", "Jones", 25));
+		employeeRepository.save(new Employee("Anna", "Williams", 28));
+		employeeRepository.save(new Employee("Joan", "Jones", 25));
+		employeeRepository.save(new Employee("Anna", "Williams", 28));
+		employeeRepository.save(new Employee("Joan", "Jones", 25));
+		employeeRepository.save(new Employee("Anna", "Williams", 28));
+		employeeRepository.save(new Employee("Joan", "Jones", 25));
+		employeeRepository.save(new Employee("Anna", "Williams", 28));
+		employeeRepository.save(new Employee("Joan", "Jones", 25));
+		Timestamp finishCrete20 = new Timestamp(System.currentTimeMillis());
+		
+		logger.info("duration create 20 operation ... (ms)" + (startCreate20.getTime() - finishCrete20.getTime()));
        
         logger.info("Data creation Employee complete...");
         
@@ -37,16 +60,35 @@ public class ApplicationCommandRunner implements CommandLineRunner {
         
         logger.info("employees count: " + employeeRepository.count());
         
-        Employee employeeFound = employeeRepository.findItemByName("Pere");
+        Timestamp startCreate = new Timestamp(System.currentTimeMillis());
+        employeeRepository.save(new Employee("Silvia", "Williams", 28));
+        Timestamp finishCreate = new Timestamp(System.currentTimeMillis());
+        
+        Timestamp startFind = new Timestamp(System.currentTimeMillis());
+        Employee employeeFound = employeeRepository.findItemByName("Silvia");
+        Timestamp finishFind = new Timestamp(System.currentTimeMillis());
         logger.info("employees Pere" + employeeFound);
         employeeRepository.deleteById( employeeFound.getId() );
+        Timestamp deleteOperation = new Timestamp(System.currentTimeMillis());
         
+        logger.info("duration create operation ... (ms)" + (startCreate.getTime() - finishCreate.getTime()));
+        logger.info("duration find ... (ms)" + (startFind.getTime() - finishFind.getTime()));
+        logger.info("duration delete operation ... (ms)" + (finishFind.getTime() - deleteOperation.getTime()));
         
         employeeRepository.deleteById("62277a52b0dcbc035e1f3e2c");
-        
-       // logger.info
-        
-     
+         
+        Timestamp startDelete = new Timestamp(System.currentTimeMillis());
+        employeeRepository.deleteAll();
+        Timestamp finishDelete = new Timestamp(System.currentTimeMillis());
+        logger.info("duration delete all operation ... (ms)" + (startDelete.getTime() - finishDelete.getTime()));
+         
+        employeeRepository.save(new Employee("Teresa", "Williams", 58));
+        employeeFound = employeeRepository.findItemByName("Teresa");
+        employeeFound.setAge(59);
+        employeeRepository.save(employeeFound);
+        employeeFound = employeeRepository.findItemByName("Teresa");
+        logger.info("update Teresa from age 58 to 59 ..." + employeeFound);
+         
 	}	
 
 }
